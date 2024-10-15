836c0ef2d993b394170345125bc79faee78769f4
SilianZ <silian_zheng@outlook.com>
2024-10-15 14:46:56 +0800

    fix: 微调字号

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -12,21 +12,21 @@ setTimeout(() => router.go(-1), 5000);
         />
         <h2 class="text-center">404</h2>
         <br />
-        <p class="text-center text-lg mb-2">
+        <p class="text-center text-md mb-2">
             Sorry, something went wrong, but we are making it right.
         </p>
-        <p class="text-center text-lg mb-2">
+        <p class="text-center text-md mb-2">
             You seem lost, don't worry, we are getting back on track.
         </p>
-        <p class="text-center text-lg mb-2">
+        <p class="text-center text-md mb-2">
             You will return to the previous page in 5 seconds.
         </p>
         <br />
-        <p class="text-center text-lg mb-2">抱歉，出错了，但我们做对了。</p>
-        <p class="text-center text-lg mb-2">
+        <p class="text-center text-md mb-2">抱歉，出错了，但我们做对了。</p>
+        <p class="text-center text-md mb-2">
             您看起来丢失了，别担心，我们正在返回轨道。
         </p>
-        <p class="text-center text-lg mb-2">将在 5 秒钟后返回上一页。</p>
+        <p class="text-center text-md mb-2">将在 5 秒钟后返回上一页。</p>
     </div>
 </template>
 
