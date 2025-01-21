387720a494356c1a350ae3a369b348e2d22e198c
SilianZ <silian_zheng@outlook.com>
2025-01-21 20:20:22 +0800

    fix: 更新 API

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,7 @@ import COS, { Credentials } from "cos-js-sdk-v5";
 axios.defaults.baseURL =
     process.env.VERCEL_ENV == "production"
         ? "https://api.hfiuc.org"
-        : "https://120.24.212.59/";
+        : "https://api.preview.hfiuc.org/";
 
 export interface ApplicationInfo {
     selectedClass: string;
