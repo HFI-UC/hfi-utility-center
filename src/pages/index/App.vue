319edd0d9d5cf7f26e9090269356f73a904fb374
SilianZ <silian_zheng@outlook.com>
2025-09-06 17:28:36 +0800

    fix: 组件越位

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -6,7 +6,7 @@ import Navbar from '../../components/Navbar.vue';
 <template>
     <LoadingMask />
     <Navbar></Navbar>
-    <div class="flex items-center justify-center h-[100svh] flex-col">
+    <div class="flex items-center justify-center h-[100%] flex-col">
         <h1 class="font-extrabold text-4xl sm:text-5xl bg-linear-to-r from-cyan-500 to-sky-500 bg-clip-text text-transparent text-center inline-block p-4">HFI Utility Center</h1>
         <h2 class="font-bold">by MAKERs'</h2>
         <div class="mt-8 flex gap-2 flex-wrap items-center justify-center">
