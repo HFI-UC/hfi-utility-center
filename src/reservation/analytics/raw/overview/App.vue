3419c08bca96b51e6d6c859db7abdd51b8de0587
SilianZ <silian_zheng@outlook.com>
2025-09-27 22:05:50 +0800

    fix: ?

--- src/reservation/analytics/raw/overview/App.vue
+++ src/reservation/analytics/raw/overview/App.vue
@@ -353,7 +353,7 @@ watch(
 <template>
     <div class="mx-[3rem]">
         <h1
-            class="mt-8 font-bold text-xl from-cyan-500 to-sky-500 bg-linear-to-r bg-clip-text text-transparent"
+            class="font-bold text-xl from-cyan-500 to-sky-500 bg-linear-to-r bg-clip-text text-transparent"
             >HFI Utility Center</h1
         >
         <h1 class="font-bold text-3xl my-4">Reservation Analytics - Overview</h1>
